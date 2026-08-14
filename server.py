caac5a234dbb6648e91de0df96d0f6afa064d299
MinukaSithumika955 <minukasithumika6910@gmail.com>
2026-08-14 14:22:32 +0530

    server.py

--- server.py
+++ server.py
@@ -1,46 +1,83 @@
-from flask import Flask, render_template, request, session, redirect, url_for
-from flask_socketio import SocketIO, emit, join_room, leave_room
-import os
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_socketio import SocketIO, join_room, emit
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__, static_folder='static')
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'randomsecretkey123')
+app.config['SECRET_KEY'] = 'flowchat_secret_key_123' # wenas karanna
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+socketio = SocketIO(app)
 
-# SocketIO - gevent use karan nisa async_mode eka auto detect wenawa
-socketio = SocketIO(app, cors_allowed_origins="*")
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-# Room walata users store karanna
-rooms = {}
+# Database Model
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+with app.app_context():
+    db.create_all() # first time witharak
 
 @app.route('/')
-def index():
+def home():
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
     return render_template('index.html')
 
-@app.route('/room/<room_id>')
-def room(room_id):
-    return render_template('room.html', room_id=room_id)
+# Register
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = generate_password_hash(request.form['password'])
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+# Login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = User.query.filter_by(username=request.form['username']).first()
+        if user and check_password_hash(user.password, request.form['password']):
+            login_user(user)
+            return redirect(url_for('dashboard'))
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('index.html', username=current_user.username)
+
+@app.route('/room/<room>')
+@login_required
+def room(room):
+    return render_template('room.html', room=room, username=current_user.username)
 
 @socketio.on('join')
 def on_join(data):
-    room = data['room']
-    join_room(room)
-    if room not in rooms:
-        rooms[room] = []
-    rooms[room].append(request.sid)
-    emit('message', {'msg': 'User joined'}, room=room)
-
-@socketio.on('leave')
-def on_leave(data):
-    room = data['room']
-    leave_room(room)
-    if room in rooms and request.sid in rooms[room]:
-        rooms[room].remove(request.sid)
-    emit('message', {'msg': 'User left'}, room=room)
+    join_room(data['room'])
 
 @socketio.on('message')
 def handle_message(data):
-    room = data['room']
-    emit('message', data, room=room)
+    emit('message', data, room=data['room'])
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app)
